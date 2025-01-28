bc337ed918eea4252f8a9c612f099902b77c2138
Renan <163222315+RenanAlbuquerqueNunes@users.noreply.github.com>
2025-01-28 17:43:21 -0300

    :zaap: merging branchs and solving conflicts

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lalezar&display=swap');
  body{
   background-color: black;
+  font-family: 'Lalelar', sans-serif;
  }
 </style>
 
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -35,5 +35,53 @@ const openDrawer = ref<boolean>(false)
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <footer class="bg-dark text-white">
+      <div class="q-pa-md">
+        <div class="row justify-between">
+          <!-- Logo e Direitos Autorais -->
+          <div class="col-3">
+            <div>
+              <img src="/logo.png" alt="Logo" style="width: 50px;" />
+            </div>
+            <p class="text-caption">
+              &copy; 2025 Perri Engenharia. Todos os direitos reservados.
+            </p>
+          </div>
+
+          <!-- Links Úteis -->
+          <div class="col-3">
+            <h6>Links Úteis</h6>
+            <ul>
+              <li><a href="/" class="text-white">Início</a></li>
+              <li><a href="/servicos" class="text-white">Serviços</a></li>
+              <li><a href="/sobre" class="text-white">Sobre Nós</a></li>
+              <li><a href="/contato" class="text-white">Contato</a></li>
+            </ul>
+          </div>
+
+          <!-- Informações -->
+          <div class="col-3">
+            <h6>Informações</h6>
+            <ul>
+              <li><a href="/localizacao" class="text-white">Localização</a></li>
+              <li><a href="/atendimento" class="text-white">Horários de Atendimento</a></li>
+              <li><a href="/faq" class="text-white">FAQ</a></li>
+              <li><a href="/privacidade" class="text-white">Política de Privacidade</a></li>
+            </ul>
+          </div>
+
+          <!-- Redes Sociais -->
+          <div class="col-3">
+            <h6>Siga-nos</h6>
+            <ul class="social-links">
+              <li><a href="#" class="text-white">Facebook</a></li>
+              <li><a href="#" class="text-white">Instagram</a></li>
+              <li><a href="#" class="text-white">YouTube</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   </q-layout>
 </template>
